82d8649f8f788e8cf00a1aa08a44942265eed80f
Máté Homolya <mate.homolya@gmail.com>
2021-11-11 09:25:01 -0700

    Use react

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+function App() {
+  return (
+    <>
+      <h1>Macoda Mattress</h1>
+      <a href="https://macoda.com.au/products/macoda-mattress" target="_blank">
+        See Main page
+      </a>
+      <div>
+        <img
+          style={{ maxWidth: "100vw", objectFit: "cover" }}
+          src="/render-2.png"
+        />
+      </div>
+    </>
+  );
+}
+
+export default App;
